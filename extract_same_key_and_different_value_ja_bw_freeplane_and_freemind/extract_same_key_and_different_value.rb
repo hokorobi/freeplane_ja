48e1e485325c7bf756ead98dcee2a91165bbf396
hokorobi <hokorobi@.(none)>
2010-03-06 22:07:13 +0900

    リファクタリングしたつもり。

--- extract_same_key_and_different_value_ja_bw_freeplane_and_freemind/extract_same_key_and_different_value.rb
+++ extract_same_key_and_different_value_ja_bw_freeplane_and_freemind/extract_same_key_and_different_value.rb
@@ -1,71 +1,55 @@
-usage = "$0 in_english_properties in_freeplane_japanese_properties in_freemind_japanese_properties out_english_properties out_freeplane_japanese_properties out_freemind_japanese_properties"
+usage = "USAGE: $0 in_english_properties in_freeplane_japanese_properties in_freemind_japanese_properties out_english_properties out_freeplane_japanese_properties out_freemind_japanese_properties"
 if ARGV.size < 6
   print usage
   exit 1
 end
 
-def properties2hash (lines)
+# ファイルを読み込んでキーと値を分離してハッシュに読み込む
+def properties2hash (filepath)
+  lines = IO.readlines(filepath)
   hash = Hash.new
   array = Array.new
   lines.each{|line|
-    if line.index(/^#/) || line.index(/^$/)
+    # コメントと空白行はスキップ
+    if line.index(/^\s*#/) || line.index(/^$/)
       next
     end
     array = line.chomp.split(/=/, 2)
     while key = array.shift
-      value = array.shift
-      if value == nil
-        value = ""
-      end
-      hash[key] = value
+      hash[key] = array.shift
     end
   }
-  hash.sort_by{|key, value| key.downcase}
   return hash
 end
 
-def hash2properties (hash_src, hash_der, hash_en, out_src, out_en, count)
-    hash_src.each{|key, value|
-      begin
-        if hash_der[key].to_s.lstrip.downcase != value.to_s.lstrip.downcase && hash_der[key] != nil && value != nil
-          out_src.print key, "=", value, "\n"
-          if count != 1
-            out_en.print key, "=", hash_en[key], "\n"
-          end
-        end
-      rescue => ex
-        p key
-        p hash_en[key]
-        p hash_src[key]
-        p hash_der[key]
-        p ex.message
-        exit 1
+# ハッシュの値を比較して同じキーで値の異なるものをファイルへ書き出す
+def hash2properties (hash_en, hash_fp, hash_fm, filepath_en, filepath_fp, filepath_fm)
+  out_en = open(filepath_en, "w")
+  out_fp = open(filepath_fp, "w")
+  out_fm = open(filepath_fm, "w")
+
+  hash_fp.each{|key, value|
+    begin
+      if hash_fm[key] != nil && hash_fp[key] != nil && hash_fm[key].to_s.lstrip.downcase != value.to_s.lstrip.downcase
+        out_fp.print key, "=", value, "\n"
+        out_fm.print key, "=", hash_fm[key], "\n"
+        out_en.print key, "=", hash_en[key], "\n"
       end
-    }
+    rescue => ex
+      p ex.message
+      exit 1
+    end
+  }
+  out_en.close
+  out_fp.close
+  out_fm.close
 end
 
-en = IO.readlines(ARGV[0])
-#p en
-hash_en = properties2hash(en)
+hash_en = properties2hash(ARGV[0])
 #p hash_en
-
-fp_ja = IO.readlines(ARGV[1])
-#p fp_ja
-hash_fp_ja = properties2hash(fp_ja)
+hash_fp = properties2hash(ARGV[1])
 #p hash_fp_ja
-
-fm_ja = IO.readlines(ARGV[2])
-#p fm_ja
-hash_fm_ja = properties2hash(fm_ja)
+hash_fm = properties2hash(ARGV[2])
 #p hash_fm_ja
 
-out_en = open(ARGV[3], "w")
-out_fp_ja = open(ARGV[4], "w")
-out_fm_ja = open(ARGV[5], "w")
-
-hash2properties(hash_fm_ja, hash_fp_ja, hash_en, out_fm_ja, out_en, 0)
-hash2properties(hash_fp_ja, hash_fm_ja, hash_en, out_fp_ja, out_en, 1)
-
-out_en.close
-out_fp_ja.close
-out_fm_ja.close
+hash2properties(hash_en, hash_fp, hash_fm, ARGV[3], ARGV[4], ARGV[5])
