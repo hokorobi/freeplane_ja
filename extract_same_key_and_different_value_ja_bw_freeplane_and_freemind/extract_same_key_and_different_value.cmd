5687920e6fb61da6b44b5f7f62d828ac15bc5c99
hokorobi <hokorobi@example.com>
2010-03-27 18:33:52 +0900

    ソートを追加

--- extract_same_key_and_different_value_ja_bw_freeplane_and_freemind/extract_same_key_and_different_value.cmd
+++ extract_same_key_and_different_value_ja_bw_freeplane_and_freemind/extract_same_key_and_different_value.cmd
@@ -1,1 +1,10 @@
 ruby extract_same_key_and_different_value.rb ..\Resources_en.properties ..\Resources_ja.properties ..\Resources_ja_fm.properties Resources_en_diff.properties Resources_ja_fp_diff.properties Resources_ja_fm_diff.properties
+sort -f Resources_en_diff.properties > Resources_en_diff.properties_
+del Resources_en_diff.properties
+rename Resources_en_diff.properties_ Resources_en_diff.properties
+sort -f Resources_ja_fp_diff.properties > Resources_ja_fp_diff.properties_
+del Resources_ja_fp_diff.properties
+rename Resources_ja_fp_diff.properties_ Resources_ja_fp_diff.properties
+sort -f Resources_ja_fm_diff.properties > Resources_ja_fm_diff.properties_
+del Resources_ja_fm_diff.properties
+rename Resources_ja_fm_diff.properties_ Resources_ja_fm_diff.properties
